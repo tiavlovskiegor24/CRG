06c2ddc5c21fbcf5d8beeed088cd451012e8639c
Egor Tiavlovsky <tiavlovskiegor@gmail.com>
2017-02-15 14:09:53 +0100

    Interface of cooler changed

--- hi_c_cooler.py
+++ hi_c_cooler.py
@@ -1,6 +1,7 @@
 #function to extract hi-c matrix from cooler file format
 import cooler
 import numpy as np
+from scipy import sparse
 
 def hi_c_cooler(filepath,chrom):
 
@@ -8,7 +9,7 @@ def hi_c_cooler(filepath,chrom):
     c = cooler.Cooler(filepath)
 
     # select indivudual chromosomes
-    cis = c.matrix().fetch(chrom).tocsr().astype(np.float)
+    cis = sparse.csr_matrix(c.matrix(balance = False).fetch(chrom)).astype(np.float)
     print "Matrix shape:",cis.shape
     print "Density %.2f"%(cis.nnz*1./cis.shape[0]**2)
     return cis
