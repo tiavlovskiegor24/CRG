4c271c4db5306e8e8104fcc7bf86ca45ea1aeae7
tiavlovskiegor <tiavlovskiegor@gmail.com>
2017-02-15 22:20:01 +0100

    Made the code of gen_feature vector much shorter

--- hic_contact_decay.py
+++ hic_contact_decay.py
@@ -2,6 +2,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 from myplot import myplot
 
+def 
+
 def process_data(data):
     data = (data+1)*1./(data+1).sum()# probability distribution using\
                #Laplace rule
@@ -16,63 +18,47 @@ def gen_dec_feature_vec(M,dbins_max = 100):
     n = M.shape[0]
     dec_vec = np.zeros(n)
     dbins = np.arange(1,dbins_max+1)
-    
+
+    # first row
     data = M[0].toarray().ravel()[1:dbins_max+1]
     data = process_data(data)
     dec_vec[0] = compute_decay(data,dbins,plot = True)[0]
     plt.title("bin %d, decay rate = %.2f"%(0,dec_vec[0]))
 
-    if n < 2:
-        return dec_vec
-    
+    # last row
     data = M[n-1].toarray().ravel()[::-1][1:dbins_max+1]
     data = process_data(data)   
     dec_vec[n-1] = compute_decay(data,dbins,plot = True)[0]
     plt.title("bin %d, decay rate = %.2f"%(n-1,dec_vec[n-1]))
 
+    if n % 2 != 0:
+        #middle row of odd number of rows
+        data = M[n/2+1].toarray().ravel()[1:dbins_max+1]
+        data = process_data(data)   
+        dec_vec[n/2-1] = compute_decay(data,dbins,plot = False)[0]
+    
+
     sample = np.sort(np.random.randint(1,n,5)).tolist()[::-1]
 
     for loc in xrange(1,n/2):
-        #print "First",loc
-        data = M[loc].toarray().ravel()
-        data[loc+1:2*loc+1] = np.maximum(data[:loc][::-1],\
-                                         data[loc+1:2*loc+1])
-        data = data[loc+1:]
+        for select in [loc,-loc-1]:
 
-        data = process_data(data)
+            data = M[select].toarray().ravel()
+            if select == -loc-1:
+                data = data[::-1]
 
-        data = data[:dbins_max]
+            data[loc+1:2*loc+1] = np.maximum(data[:loc][::-1],\
+                                             data[loc+1:2*loc+1])
+            data = data[loc+1:]
+            data = process_data(data)
+            data = data[:dbins_max]
 
-        if sample:
-            if loc == sample[-1]:
-                 dec_vec[loc] = compute_decay(data,dbins,plot=True)[0]
-                 plt.title("bin %d, decay rate = %.2f"%(loc,dec_vec[loc]))
+            if select in sample or n+select in sample:
+                 dec_vec[select] = compute_decay(data,dbins,plot=True)[0]
+                 plt.title("bin %d, decay rate = %.2f"%(select,dec_vec[select]))
                  sample.pop()
-                 continue
-        
-        dec_vec[loc] = compute_decay(data,dbins)[0]
-            
-    for loc in xrange(n/2,n-1):
-        #print "Second",loc
-        data = M[loc].toarray().ravel()
-        data[loc-(n-loc-1):loc] = np.maximum(data[loc-(n-loc-1):loc],\
-                                             data[loc+1:][::-1])
-
-        data = data[:loc][::-1]
-
-        data = process_data(data)
-
-
-        data = data[:dbins_max]
-
-        if sample:
-            if loc == sample[-1]:
-                 dec_vec[loc] = compute_decay(data,dbins,plot=True)[0]
-                 sample.pop()
-                 plt.title("bin %d, decay rate = %.2f"%(loc,dec_vec[loc]))
-                 continue
-        
-        dec_vec[loc] = compute_decay(data,dbins)[0]
+            else:
+                dec_vec[select] = compute_decay(data,dbins)[0]
          
     return dec_vec
     
